b1288c9e24250d310e5ce0208ab6d3c094dea5da
nld789 <nld789@hotmail.com>
2014-09-22 01:45:35 +0700

    - Add search advance.

--- web/Scripts/plugins/action/Payment/action.PAY010_1.js
+++ web/Scripts/plugins/action/Payment/action.PAY010_1.js
@@ -1,7 +1,64 @@
-
 $(function() {
+    
+    onClickSearchAdvance = function() {
+        var search = {};
+        var statusSearch = false; //0 is not in condition, 1 is in condition
+        var requestSearch = new Array();
+        var condition = '';
+        
+        var memberCode = $('#memberCode').val();
+        if (memberCode) {
+            requestSearch.push({'groupOp': condition, 'field': 'memberCode', 'op': 'eq', 'data': memberCode, 'dataType': 'varchar'});
+            condition = 'and';
+            statusSearch = true;
+        }
+        
+        var idCard = $('#idCard').val();
+        if (idCard) {
+            requestSearch.push({'groupOp': condition, 'field': 'citizenId', 'op': 'eq', 'data': idCard, 'dataType': 'varchar'});
+            condition = 'and';
+            statusSearch = true;
+        }
+        
+        var name = $('#name').val();
+        if (name) {
+            requestSearch.push({'groupOp': condition, 'field': 'name', 'op': 'eq', 'data': name, 'dataType': 'varchar'});
+            condition = 'and';
+            statusSearch = true;
+        }
+        
+        var surname = $('#surname').val();
+        if (surname) {
+            requestSearch.push({'groupOp': condition, 'field': 'surname', 'op': 'eq', 'data': surname, 'dataType': 'varchar'});
+            condition = 'and';
+            statusSearch = true;
+        }
+        
+        var militaryDeptId = $('#militaryDeptId').val();
+        if (militaryDeptId) {
+            requestSearch.push({'groupOp': condition, 'field': 'militaryId', 'op': 'eq', 'data': militaryDeptId, 'dataType': 'integer'});
+            condition = 'and';
+            statusSearch = true;
+        }
+        
+        var status = $('#status').val();
+        if (status) {
+            requestSearch.push({'groupOp': condition, 'field': 'memberStatusCode', 'op': 'eq', 'data': status, 'dataType': 'integer'});
+            condition = 'and';
+            statusSearch = true;
+        }
 
-    onActionLoadMilitaryDepartment = function() {
+        search.conditions = requestSearch;
+        $(gridName).jqGrid('setGridParam', {
+            search: statusSearch,
+            postData: {
+                searchCommand: $.toJSON(search)
+            }
+        });
+        $(gridName).trigger("reloadGrid", [{page: 1}]);
+    };
+    
+    onLoadMilitaryDepartment = function () {
         var objData = {};
         $.ajax({
             type: 'POST',
@@ -11,37 +68,143 @@ $(function() {
             async: false,
             data: objData,
             dataType: 'json',
-            success: function(json) {
-                listMilitaryDepartment = json;
-                onActionLoadNewMilitaryDepartment();
+            success: function (listMilitaryDepartment) {
+                $('#militaryDeptId').empty();
+                $('#militaryDeptId').append('<option value="">ทั้งหมด</option>');
+                for (var item in listMilitaryDepartment) {
+                    var itemData = listMilitaryDepartment[item];
+                    $('#militaryDeptId').append('<option value="' + itemData.militaryId + '">' + itemData.name + '</option>');
+                }
             },
-            error: function(XMLHttpRequest, textStatus, errorThrown) {
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
                 $.fn.MessageError(XMLHttpRequest, textStatus, errorThrown);
             },
-            beforeSend: function(jqXHR) {
+            beforeSend: function (jqXHR) {
             }
         });
     };
 
-    onActionClearNewMilitaryDepartment = function() {
-        $('#military').empty();
-    };
+    onClickSearch = function(memberId) {
+        var search = {};
+        if(memberId !== null) {
+            // get memberId from search advance
+            var requestSearch = new Array();
+            requestSearch.push({'groupOp': '', 'field': 'memberId', 'op': 'eq', 'data': memberId, 'dataType': 'varchar'});
+            search.conditions = requestSearch;
 
-    onActionLoadNewMilitaryDepartment = function() {
-        onActionClearNewMilitaryDepartment();
-        $('#military').append('<option value="">ทั้งหมด</option>');
-        for (var item in listMilitaryDepartment) {
-            var itemData = listMilitaryDepartment[item];
-            $('#military').append('<option value="' + itemData.militaryId + '">' + itemData.name + '</option>');
+            searchMember($.toJSON(search));
+        } else {
+            // click quick search between Citizenid or Membercode
+            var txtSearch = $("#txtSearch").val();
+            if (txtSearch) {
+                var field;
+                if ($("#searchTypeCitizenid").prop("checked", true)) {
+                    field = "citizenId";
+                }
+                if ($("#searchTypeMemberCode").prop("checked", true)) {
+                    field = "memberCode";
+                }
+
+                if (field !== "undefined") {
+                    var requestSearch = new Array();
+                    requestSearch.push({'groupOp': '', 'field': 'memberCode', 'op': 'eq', 'data': txtSearch, 'dataType': 'varchar'});
+                    search.conditions = requestSearch;
+
+                    searchMember($.toJSON(search));
+                }
+            } else {
+                $("#Dialog-Confirm").html("กรุณากรอกข้อมูลการค้นหา");
+                $("#Dialog-Confirm").removeClass('hide').dialog({
+                    width: '300px',
+                    resizable: false,
+                    modal: true,
+                    title: "<div class='widget-header'><h4 class='smaller'> แจ้งเตือน</h4></div>",
+                    title_html: true,
+                    autoOpen: true,
+                    buttons: [{
+                        html: "<i class='ace-icon fa fa-times bigger-110'></i>&nbsp; ปิด",
+                        "class": "btn btn-xs",
+                        click: function() {
+                            $(this).dialog("close");
+                        }
+                    }]
+                });
+            }
         }
     };
 
-    init = function() {
+    searchMember = function(objData) {
+        $.ajax({
+            type: 'POST',
+            url: urlListMember,
+            data: objData,
+            dataType: 'json',
+            async: true,
+            success: function(msg) {
+                alert("msg=" + msg);
+                if (msg.checkSuccess === true && msg.obj !== null) {
+                    var object = msg.obj;
+                    //alert("status : >>" + object.status + "<<");
+                    if (object.status === 10 || object.status === 11) {
+                        $("#lblMemberCode").text(object.memberCode);
+                        $("#lblCitizenId").text(object.citizenId);
+                        $("#lblName").text(object.rankOrTitleName + object.name + " " + object.surname);
+                        $("#lblMilitaryName").text(object.militaryName);
+                    } else {
+                        $("#Dialog-Confirm").html("ไม่พบข้อมูล");
+                        $("#Dialog-Confirm").removeClass('hide').dialog({
+                            width: '300px',
+                            resizable: false,
+                            modal: true,
+                            title: "<div class='widget-header'><h4 class='smaller'><i class='ace-icon glyphicon glyphicon-trash'></i> สถานะ</h4></div>",
+                            title_html: true,
+                            autoOpen: true,
+                            buttons: [
+                                {
+                                    html: "<i class='ace-icon fa fa-times bigger-110'></i>&nbsp; ปิด",
+                                    "class": "btn btn-xs",
+                                    click: function() {
+                                        $(this).dialog("close");
+                                    }
+                                }
+                            ]
+                        });
+                    }
+                } else {
+                    $.fn.DialogMessage(msg);
+                }
 
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                $.fn.MessageError(XMLHttpRequest, textStatus, errorThrown);
+            }
+        });
+    };
+
+    $("#btnSearch").click(function(event) {
+        event.preventDefault();
+        onClickSearch(null);
+    });
+
+    $("#btnSearchAdvance").click(function(event) {
+        event.preventDefault();
+        onClickSearchAdvance(null);
+    });
+
+    $("#btnSearchAdvanceSelect").click(function(event) {
+        event.preventDefault();
+        onClickSearch($("input:radio[name=selectMember]:checked").val());
+    });
+
+    $("#btnClearAdvance").click(function(event) {
+        event.preventDefault();
+        $('#paymentDate').val("");
+    });
+
+    init = function() {
         $('#paymentDate').datepicker({language: 'th', format: 'dd/mm/yyyy'});
         $("#paymentDate").datepicker("setDate", new Date());
-
-        onActionLoadMilitaryDepartment();
+        onLoadMilitaryDepartment();
     };
 
     init();
--- web/Scripts/plugins/jqgrid/Payment/jqgrid.PAY010_1.js
+++ web/Scripts/plugins/jqgrid/Payment/jqgrid.PAY010_1.js
@@ -1,4 +1,124 @@
+var gridUrl = urlListMember;
+var gridName = '#gridData_MemberPaymentGrid_List';
+var gridPager = '#gridPager_MemberPaymentGrid_List';
+var gridSortName = 'memberId';
+var gridSortOrder = 'asc';
+var gridCaption = 'ข้อมูลสมาชิก';
+var gridColNames = ['', 'หน่วยต้นสังกัด', 'เลขทะเบียนสมาชิก', 'เลขประจำตัวประชาชน', 'ยศ - คำนำหน้า', 'ชื่อ', 'สกุล', 'สถานะ'];
+var gridColModel = [
+    {name: 'memberId', index: 'memberId', align: 'center', sortable: false, width: 40,
+        formatter: function (cellVal, opts, rowObject, action) {
+            return '<input type="radio" name="selectMember" id="member_' + cellVal + '" value="' + cellVal + '"/>';
+        }
+    },
+    {name: 'militaryName', index: 'militaryName', sortable: false, width: 150},
+    {name: 'memberCode', index: 'memberCode', align: 'center', sortable: false, width: 100},
+    {name: 'citizenId', index: 'citizenId', align: 'center', sortable: false, width: 150},
+    {name: 'rankOrTitleName', index: 'rankOrTitleName', sortable: false, width: 120},
+    {name: 'name', index: 'name', sortable: false, width: 100},
+    {name: 'surname', index: 'surname', sortable: false, width: 100},
+    {name: 'memberStatusCode', index: 'memberStatusCode', sortable: false, width: 145}
+];
+
+var gridJsonReader = {
+    records: "records", //total number of records for the query
+    repeatitems: false,
+    id: "memberId"           //the unique id of the row
+};
+var updatePagerIcons = function(table) {
+    var replacement =
+            {
+                'ui-icon-seek-first': 'ace-icon fa fa-angle-double-left bigger-140',
+                'ui-icon-seek-prev': 'ace-icon fa fa-angle-left bigger-140',
+                'ui-icon-seek-next': 'ace-icon fa fa-angle-right bigger-140',
+                'ui-icon-seek-end': 'ace-icon fa fa-angle-double-right bigger-140'
+            };
+    $('.ui-pg-table:not(.navtable) > tbody > tr > .ui-pg-button > .ui-icon').each(function() {
+        var icon = $(this);
+        var $class = $.trim(icon.attr('class').replace('ui-icon', ''));
+
+        if ($class in replacement)
+            icon.attr('class', 'ui-icon ' + replacement[$class]);
+    });
+};
+
+var enableTooltips = function(table) {
+    $('.navtable .ui-pg-button').tooltip({container: 'body'});
+    $(table).find('.ui-pg-div').tooltip({container: 'body'});
+};
+
+var updatePagerIcons = function(table) {
+    var replacement =
+        {
+            'ui-icon-seek-first': 'ace-icon fa fa-angle-double-left bigger-140',
+            'ui-icon-seek-prev': 'ace-icon fa fa-angle-left bigger-140',
+            'ui-icon-seek-next': 'ace-icon fa fa-angle-right bigger-140',
+            'ui-icon-seek-end': 'ace-icon fa fa-angle-double-right bigger-140'
+        };
+    $('.ui-pg-table:not(.navtable) > tbody > tr > .ui-pg-button > .ui-icon').each(function() {
+        var icon = $(this);
+        var $class = $.trim(icon.attr('class').replace('ui-icon', ''));
+
+        if ($class in replacement)
+            icon.attr('class', 'ui-icon ' + replacement[$class]);
+    });
+};
 
 $(document).ready(function() {
-   
+    $(gridName).jqGrid({
+        url: gridUrl,
+        datatype: 'json',
+        mtype: 'POST',
+        caption: gridCaption,
+        colNames: gridColNames,
+        colModel: gridColModel,
+        jsonReader: gridJsonReader,
+        autowidth: true,
+        shrinkToFit: false,
+        pager: gridPager,
+        height: 'auto',
+        width: 'auto',
+        rowNum: 10,
+        sortname: gridSortName,
+        sortorder: gridSortOrder,
+        viewrecords: true,
+        multiselect: false,
+        rownumbers: false,
+        gridview: true,
+        hidegrid: false,
+        onCellSelect: function (rowid, iCol, cellcontent, e) {
+            var cm = $(gridName).jqGrid("getGridParam", "colModel");
+            if ("cb" !== cm[iCol].name && "action" !== cm[iCol].name) {
+
+            }
+        },
+        onSelectRow: function (id, event) {
+
+        },
+        ondblClickRow: function (id, rowid, colid, e) {
+        },
+        loadComplete: function () {
+
+            var ids = $(gridName).jqGrid('getDataIDs');
+            for (var i = 0; i < ids.length; i++) {
+                var id = ids[i];
+                var buttonStatus = '';
+                var buttonDelete = '<button type="button" class="btn btn-xs btn-danger" alt="Delete" onclick="onDialogDelete(\'' + id + '\');"><i class="ace-icon fa fa-trash-o bigger-120"></i> </button>';
+                $(gridName).setRowData(id, {action: buttonDelete, printedStatus: buttonStatus});
+            }
+            enableTooltips(this);
+            //styleCheckbox(this);
+            updatePagerIcons(this);
+        }
+    });
+    $(gridName).jqGrid('navGrid', gridPager, {edit: false, add: false, del: false, search: false, refresh: false},
+    {}, // edit options  
+            {}, // add options  
+            {}, //del options  
+            {
+                closeOnEscape: true,
+                multipleSearch: true,
+                closeAfterSearch: true
+            }
+    );
 });
